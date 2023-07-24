143b3791d84efba24a80a30b54d4e43e87710b68
Alan Hughes <hughesno1@gmail.com>
2023-07-24 17:03:13 +0100

    Initial Commit

---
+++ CRSQLiteFramework/CRSQLiteFramework.h
@@ -0,0 +1,18 @@
+//
+//  CRSQLiteFramework.h
+//  CRSQLiteFramework
+//
+//  Created by Alan Hughes on 24/07/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CRSQLiteFramework.
+FOUNDATION_EXPORT double CRSQLiteFrameworkVersionNumber;
+
+//! Project version string for CRSQLiteFramework.
+FOUNDATION_EXPORT const unsigned char CRSQLiteFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRSQLiteFramework/PublicHeader.h>
+
+
